50db0d38bfaf6ad0a009587c077ce88674c44725
ruffy <ruffy.developer@gmail.com>
2019-03-15 14:53:28 +0100

    Start implementing validation of user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 31
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 31
+    assert_not @user.valid?
+  end
+
+  test "email validation should work for valid addresses" do
+    valid_addresses = %w[user@email.com U_a-s_r534@gmail.com alice@GMAIL.com]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should fail for invalid addresses" do
+    invalid_addresses = %w[user@email,com U_a-s_r534_gmail.com alice@GMAIL+etc.com]
+    invalid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert_not @user.valid?, "#{valid_address.inspect} should be invalid"
+    end
+  end
 end
