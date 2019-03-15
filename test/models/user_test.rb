12f657c3ca6ff030aa8e394b85d2e21694f73476
ruffy <ruffy.developer@gmail.com>
2019-03-15 16:37:09 +0100

    Finish up password for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                    password: "any", password_confirmation: "any")
+                    password: "anyany", password_confirmation: "anyany")
   end
 
   test "should be valid" do
@@ -59,6 +59,16 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     @user.reload
     assert_equal @user.email, @user.email.downcase
+  end
+
+  test "password should be prenset (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
 
