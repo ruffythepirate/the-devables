ff737c96b38c4f0e6b10e549e98059e44db89353
ruffy <ruffy.developer@gmail.com>
2019-03-15 18:06:49 +0100

    Working on signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'users/new'
+  match 'signup', to: 'users#new', via: 'get'
+  match 'signup', to: 'users#create', via: 'post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :thoughts
+  resources :users
 
   root 'thoughts#index'
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+      email: "user@invlid",
+      password: "foo",
+      password_confirmation: "bar"}}
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+  end
+
+  test "valid signup" do
+  assert_difference 'User.count', 1 do
+    post users_path, params: { user: { name: "my username",
+                                       email: "user@valid.com",
+      password: "foofoo",
+      password_confirmation: "foofoo"}}
+    end
+  end
+end
+
