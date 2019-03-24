05d8727cb293eea6c697a8a3909da7bdd92fb503
ruffy <ruffy.developer@gmail.com>
2019-03-24 19:05:19 +0100

    Fix some minor logout bugs.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,5 +70,9 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false if no digest exists." do
+    assert_not @user.authenticated?('')
+  end
 end
 
