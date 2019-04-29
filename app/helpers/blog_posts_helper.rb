06428bd4bafb94965568c0f4f8b075ff54371605
ruffy <ruffy.developer@gmail.com>
2019-04-29 14:23:05 +0200

    Added syntax highlighting by using rouge.

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -1,3 +1,16 @@
+require 'redcarpet'
+require 'rouge'
+require 'rouge/plugins/redcarpet'
+
+class HTML < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet # yep, that's it.
+
+  def block_code(code, language)
+    content = Rouge.highlight(code, language || 'text', 'html')
+    '<pre class="highlight">' + content + '</pre>'
+  end
+end
+
 module BlogPostsHelper
   def markdown(text)
     options = {
@@ -10,11 +23,14 @@ module BlogPostsHelper
 
     extensions = {
       autolink:           true,
+      highlight:          true,
       superscript:        true,
-      disable_indented_code_blocks: true
+      #disable_indented_code_blocks: true,
+      space_after_headers: true,
+      fenced_code_blocks: true
     }
 
-    renderer = Redcarpet::Render::HTML.new(options)
+    renderer = HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
 
     markdown.render(text).html_safe
